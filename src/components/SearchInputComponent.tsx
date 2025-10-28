98aba0f7638c490ed43cae21827e9b0a39e84259
Alejandro <alejandro.rodriguez@orium.com>
2025-10-28 15:06:08 -0700

    fix: adjust inputing more than 1 item in the sale

--- src/components/SearchInputComponent.tsx
+++ src/components/SearchInputComponent.tsx
@@ -1,14 +1,17 @@
 "use client";
-import { Product } from "@/utils/types";
+import { useSale } from "@/hooks/sales";
+import { Product, SaleItem } from "@/utils/types";
 import { useEffect, useRef } from "react";
 import { mutate } from "swr";
 
 interface SearchInputComponentProps {
   initialSaleId: string;
 }
-export const SearchInputComponent = ({ initialSaleId }: SearchInputComponentProps) => {
+export const SearchInputComponent = ({
+  initialSaleId,
+}: SearchInputComponentProps) => {
   const ref = useRef<HTMLInputElement>(null);
-
+  const { data: sale } = useSale(initialSaleId);
   useEffect(() => {
     // Auto focus input field on component mount
     ref.current?.focus();
@@ -19,52 +22,86 @@ export const SearchInputComponent = ({ initialSaleId }: SearchInputComponentProp
     }, 10000);
     return () => clearInterval(autoFocusInterval);
   }, []);
-  
+
+  async function updateItemQuantity(itemInSale: SaleItem) {
+    try {
+      const increaseQuantity = await fetch(
+        `/api/v1/sales/${initialSaleId}/items/${itemInSale.id}`,
+        {
+          method: "PUT",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            quantity: itemInSale.quantity + 1,
+          }),
+        }
+      );
+      if (increaseQuantity.ok) {
+        mutate(["sales", initialSaleId]);
+      }
+      ref.current!.value = "";
+    } catch (error) {
+      console.error("Error updating item quantity:", error);
+    }
+  }
+
   async function handleSearchProduct() {
     const barcode = ref.current?.value;
     if (!barcode) {
       alert("Por favor, ingrese un código de barras");
       return;
     }
-    const result = await fetch(`/api/v1/products/code/${barcode}`, {
-      method: 'GET',
-      credentials: 'include',
-    });
-    const searchProductResponse: { data: Product[] } = await result.json();
-
-    if (result.status === 404) {
-      alert("Producto no encontrado");
-    } else if (result.status !== 200) {
-      alert("Error al buscar el producto, por favor intente de nuevo");
-    }
-    if (searchProductResponse.data.length > 1) {
-      alert(
-        "Se encontraron múltiples productos con el mismo código de barras. Por favor, verifique el código de barras."
-      );
-      return;
-    } else if (searchProductResponse.data.length === 0) {
-      alert(
-        "No se encontraron productos con el código de barras proporcionado."
-      );
-    }
-    const firstProduct = searchProductResponse.data[0];
-
-    const addItemResponse = await fetch(`/api/v1/sales/${initialSaleId}/items`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        productId: firstProduct.id,
-        quantity: 1,
-      }),
-    });
-    if (addItemResponse.ok) {
-      mutate(["sales", initialSaleId]);
-      ref.current!.value = "";
+    const itemInSale = sale?.saleProducts.find(
+      (sp) => sp?.product?.barCode === barcode
+    );
+    if (itemInSale) {
+      await updateItemQuantity(itemInSale);
     } else {
-      alert("Error al agregar el producto a la venta");
+      const result = await fetch(`/api/v1/products/code/${barcode}`, {
+        method: "GET",
+        credentials: "include",
+      });
+      const searchProductResponse: { data: Product[] } = await result.json();
+
+      if (result.status === 404) {
+        alert("Producto no encontrado");
+      } else if (result.status !== 200) {
+        alert("Error al buscar el producto, por favor intente de nuevo");
+      }
+      if (searchProductResponse.data.length > 1) {
+        alert(
+          "Se encontraron múltiples productos con el mismo código de barras. Por favor, verifique el código de barras."
+        );
+        return;
+      } else if (searchProductResponse.data.length === 0) {
+        alert(
+          "No se encontraron productos con el código de barras proporcionado."
+        );
+      }
+      const firstProduct = searchProductResponse.data[0];
+
+      const addItemResponse = await fetch(
+        `/api/v1/sales/${initialSaleId}/items`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            productId: firstProduct.id,
+            quantity: 1,
+          }),
+        }
+      );
+      if (addItemResponse.ok) {
+        mutate(["sales", initialSaleId]);
+        ref.current!.value = "";
+      } else {
+        alert("Error al agregar el producto a la venta");
+      }
     }
   }
 
