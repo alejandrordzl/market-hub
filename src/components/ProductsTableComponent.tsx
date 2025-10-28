dd8d08c941309d18e9833e2bff4ac98693e3784a
Alejandro <alejandro.rodriguez@orium.com>
2025-10-28 13:22:29 -0700

    style: Adjust delete button cursor

--- src/components/ProductsTableComponent.tsx
+++ src/components/ProductsTableComponent.tsx
@@ -70,7 +70,7 @@ export function ProductsTableComponent({ initialSaleId }: ProductsTableComponent
                 ${((item.product?.price || 0) * item.quantity).toFixed(2)}
               </td>
               <td className="px-4 md:px-6 py-2">
-                <button onClick={()=> deleteItemFromSale(item.id)} className="text-red-500 hover:text-red-700">Eliminar</button>
+                <button onClick={()=> deleteItemFromSale(item.id)} className="text-red-500 hover:text-red-700 hover:cursor-pointer">Eliminar</button>
               </td>
             </tr>
           ))
